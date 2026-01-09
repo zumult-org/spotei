7874ae582f10392d103e039e8f070c7faff79362
berndmoos <bernd.moos@googlemail.com>
2026-01-09 14:36:46 +0100

    Initial commit

---
+++ src/main/java/de/linguistibits/spotei/Spotei.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package de.linguistibits.spotei;
+
+/**
+ *
+ * @author bernd
+ */
+public class Spotei {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
