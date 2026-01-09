3c6fb5e93930eaeba48f020ffceb3a914275aa64
berndmoos <bernd.moos@googlemail.com>
2026-01-09 14:46:12 +0100

    Test commit

--- src/main/java/de/linguistibits/spotei/Spotei.java
+++ src/main/java/de/linguistibits/spotei/Spotei.java
@@ -11,6 +11,6 @@ package de.linguistibits.spotei;
 public class Spotei {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello SPOTEI!");
     }
 }
